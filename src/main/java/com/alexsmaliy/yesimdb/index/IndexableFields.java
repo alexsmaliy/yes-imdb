0dc775661decfcf0c71bd8104c48a05f3b24a839
Alex Smaliy <alex.smaliy@gmail.com>
2019-11-14 15:54:57 -0800

    constants classes

---
+++ src/main/java/com/alexsmaliy/yesimdb/index/IndexableFields.java
@@ -0,0 +1,8 @@
+package com.alexsmaliy.yesimdb.index;
+
+public class IndexableFields {
+    public static final String TITLE_FIELD = "title";
+    public static final String CAST_CREW_NAME_FIELD = "name";
+
+    private IndexableFields() { /* disallow instances */ }
+}
---
+++ src/main/java/com/alexsmaliy/yesimdb/index/Indexes.java
@@ -0,0 +1,7 @@
+package com.alexsmaliy.yesimdb.index;
+
+public class Indexes {
+    public static final String PRIMARY_INDEX_NAME = "primary-index";
+
+    private Indexes() { /* disallow instances */ }
+}
